9c18245a6bcc0c35fc2457b378466ca021bfd121
camtune <camtunemusic@icloud.com>
2024-01-19 22:41:51 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function getOption() {
+    selectElement = 
+    document.querySelector('#select1');
+    output = selectElement.value;
+    document.querySelector('.output').textContent = output;
+}
+
+
+
